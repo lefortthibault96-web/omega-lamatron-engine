14149572fbb29f210b25ca0f14176dba41f671dd
lefortthibault96-web <lefort.thibault.96@gmail.com>
2025-11-21 18:04:02 +0100

    Update start.bat
    fixed LAMATRON path

--- start.bat
+++ start.bat
@@ -1,9 +1,10 @@
 @echo off
 REM Activate the Python virtual environment
-call "E:\Users\Tibo\Obsidian\PNJisme\PNJisme\Risus\ttrpg_env\Scripts\activate.bat"
+call "E:\Users\Tibo\Obsidian\PNJisme\PNJisme\LAMATRON\ttrpg_env\Scripts\activate.bat"
 
 REM Run the TTRPG agent
-python "E:\Users\Tibo\Obsidian\PNJisme\PNJisme\Risus\ollama_ttrpg_agent.py"
+python "E:\Users\Tibo\Obsidian\PNJisme\PNJisme\LAMATRON\ollama_ttrpg_agent.py"
 
 REM Pause so you can see any messages when it exits
 pause
+
