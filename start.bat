e3e503acc7398609c5c5d6f206cd86971afc8940
lefortthibault96-web <lefort.thibault.96@gmail.com>
2025-12-18 15:06:35 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,9 +4,11 @@ cd /d "%~dp0"
 
 REM Activate the virtual environment
 call ttrpg_env\Scripts\activate
-
+REM Install requirement
+pip install -r requirements.txt
 REM Run the TTRPG agent using the venv Python
 python ollama_ttrpg_agent.py
 
 REM Pause so you can see messages when it exits
+
 pause
